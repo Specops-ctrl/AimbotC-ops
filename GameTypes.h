cb95baf678e9882b7c7bea17fccc74cff2200949
Specops-ctrl <sccapeter@gmail.com>
2025-01-10 10:11:45 -0500

    Update GameTypes.h

--- GameTypes.h
+++ GameTypes.h
@@ -121,8 +121,8 @@ struct AimbotCfg {
     std::vector<BodyPart> aimBones = {HEAD, CHEST}; // Target head, chest
     bool aimbotSmooth = true;
     float smoothAmount = 0.0f;
-    bool fovCheck = false; // Disable FOV checks
-    float fovValue = 60.0f; // Small FOV for more accurate shots
+    bool fovCheck = true; // Enable FOV checks
+    float fovValue = 30.0f; // Smaller FOV for more accurate shots
     bool drawFov = true;
     bool onShoot = true;
     bool triggerbot = true;
